ee1e99c8a7281bca358d4177d639023beb4757ac
luiz-fischer <luiz-fischer@hotmail.com>
2021-10-29 09:26:10 -0300

    ok

--- src/js/script.js
+++ src/js/script.js
@@ -1,42 +1,42 @@
 /* Carregar o menu lateral */
 $.get("navbar.html", function (data) {
-    $("#nav-placeholder").replaceWith(data);
+  $("#nav-placeholder").replaceWith(data);
 });
- 
+
 /* Função Sub Menu */
 $(document).on("click", ".side-menu > ul > li", function (e) {
-    if ($(this).find(".sub-menu").length > 0) {
-      e.preventDefault();
-      e.stopPropagation();
-      $(this).toggleClass("active");
-      $(this).find(".sub-menu").stop().slideToggle();
+  if ($(this).find(".sub-menu").length > 0) {
+    e.preventDefault();
+    e.stopPropagation();
+    $(this).toggleClass("active");
+    $(this).find(".sub-menu").stop().slideToggle();
+  }
+});
+
+/* Adicionar Form Familiar */
+$(document).ready(function () {
+  var max_fields = 5;
+  var wrapper = $(".container-familiar");
+  var add_button = $(".add_form_field");
+
+  var x = 1;
+  $(add_button).click(function (e) {
+    e.preventDefault();
+
+    if (x < max_fields) {
+      x++;
+      $.get("familiar_adicional.html", function (data) {
+        $(wrapper).append(data);
+      });
+    } else {
+      alert("Limite Máximo de 4 Campos");
     }
   });
 
-  /* Adicionar Form Familiar */ 
-  $(document).ready(function() {
-    var max_fields = 5;
-    var wrapper = $(".container-familiar");
-    var add_button = $(".add_form_field");
-
-    var x = 1;
-    $(add_button).click(function(e) {
-        e.preventDefault();
-        
-        if (x < max_fields) {
-            x++;
-            $.get("familiar_adicional.html", function (data) {
-              $(wrapper).append(data); 
-            });
-        } else {
-            alert('Limite Máximo de 4 Campos')
-        }
-    });
-    
-    /* Remove Form Familiar */
-    $(wrapper).on("click", ".delete", function(e) {
-        e.preventDefault();
-        $(this).parents('#form_familiar').remove();
-        x--;
-    })
+  /* Remove Form Familiar */
+  $(wrapper).on("click", ".delete", function (e) {
+    e.preventDefault();
+    $(this).parents("#form_familiar").remove();
+    x--;
+  });
 });
