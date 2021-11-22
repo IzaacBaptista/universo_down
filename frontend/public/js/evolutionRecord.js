14fde0a3e212aeffa729b3790ac665406babb8d0
luiz-fischer <luiz.fischer@alunos.sc.senac.br>
2021-11-21 22:39:28 -0300

    feat: add check form and date

--- frontend/public/js/evolutionRecord.js
+++ frontend/public/js/evolutionRecord.js
@@ -1,20 +1,22 @@
-
 var baseUrlApi = "http://localhost:3000/api/v1";
-  
-$(document).ready(function () {
-    //height
-    $("#height").mask("9.99");
- 
-    window.localStorage.removeItem("assistedId");
-    $("#save-btn").click(function (e) {
-      e.preventDefault();
-      var assistedId = localStorage.getItem("assistedId");
-      var date = document.getElementById("date");
-      var status = $("#status :selected").val();
-      var weight = document.getElementById("weight");
-      var height = document.getElementById("height");
-      var report = document.getElementById("report");
+import { erroHandler } from "../js/fns/erroHandler.js";
+import { checkProperties } from "../js/fns/checkForm.js";
 
+$(document).ready(function () {
+  //height
+  $("#height").mask("9.99");
+
+  window.localStorage.removeItem("assistedId");
+  $("#save-btn").click(function (e) {
+    var assistedId = localStorage.getItem("assistedId");
+    var date = document.getElementById("date");
+    var status = $("#status :selected").val();
+    var weight = document.getElementById("weight");
+    var height = document.getElementById("height");
+    var report = document.getElementById("report");
+    var dateNow = new Date();
+    var data = dateNow.toLocaleDateString('en');
+    if ( date.value >= data) {
       var data = {
         assistedId: assistedId,
         date: date.value,
@@ -23,45 +25,42 @@ $(document).ready(function () {
         height: height.value,
         report: report.value,
       };
-      console.log("UOBA" + JSON.stringify(data));
-      $.ajax({
-        url: baseUrlApi + "/evolution-record",
-        type: "POST",
-        headers: {
-          "x-access-token": localStorage.getItem("Authorization"),
-        },
-        cache: false,
-        contentType: "application/json",
-        dataType: "JSON",
-        data: JSON.stringify(data),
-        success: function (data) {
-          if (data != "") {
-            alert("Cadastro Realizado!");
+    } else {
+      alert("Data Inválida");
+
+    }
+    console.log("DATE " + data + "  DATA  " + date.value);
+    
+    console.log("UOBA" + JSON.stringify(data));
+    try {
+      if (checkProperties(data) === false) {
+        $.ajax({
+          url: baseUrlApi + "/evolution-record",
+          type: "POST",
+          headers: {
+            "x-access-token": localStorage.getItem("Authorization"),
+          },
+          cache: false,
+          contentType: "application/json",
+          dataType: "JSON",
+          data: JSON.stringify(data),
+          success: function (data) {
+            if (data != "") {
+              alert("Cadastro Realizado!");
+              location.reload(true);
+            }
+          },
+          error: function (err) {
             location.reload(true);
-          }
-        },
-        error: function (err) {
-          location.reload(true);
-          switch (err.status) {
-            case 304:
-              alert("Sem Alteração!!");
-              break;
-            case 400:
-              alert("Estrutura de requisição inválida!!");
-              break;
-            case 401:
-              alert("Usuário não possui permissão para esta ação!");
-              break;
-            case 500:
-              alert(
-                "O servidor encontrou uma situação com a qual não sabe lidar"
-              );
-              break;
-            default:
-              alert("Erro Desconhecido" + err.status);
-          }
-        },
-      });
+            erroHandler(err.status);
+          },
+        });
+      } else {
+        alert("Campo(s) em branco");
+      }
+    } catch (error) {
+      erroHandler(error.status);
+      alert("Erro ao cadastrar " + error);
+    }
   });
 });
-  
