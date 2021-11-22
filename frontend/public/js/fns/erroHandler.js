1835ab7300305a62522fd4ea7fa2d485671f4312
luiz-fischer <luiz.fischer@alunos.sc.senac.br>
2021-11-21 22:08:48 -0300

    feat: check prop on form

--- frontend/public/js/fns/erroHandler.js
+++ frontend/public/js/fns/erroHandler.js
@@ -1,18 +1,21 @@
 export function erroHandler(err) {
     switch (err) {
+      case 200:
+        return alert("???");
+        break;
       case 304:
-        alert("Sem Alteração!!");
+        return alert("Sem Alteração!!");
         break;
       case 400:
-        alert("Estrutura de requisição inválida!!");
+        return alert("Estrutura de requisição inválida!!");
         break;
       case 401:
-        alert("Usuário não possui permissão para esta ação!");
+        return alert("Usuário não possui permissão para esta ação!");
         break;
       case 500:
-        alert("O servidor encontrou uma situação com a qual não sabe lidar");
+        return alert("O servidor encontrou uma situação com a qual não sabe lidar");
         break;
       default:
-        alert("Erro Desconhecido: " + err);
+        return alert("Erro Desconhecido: " + err);
     }
   }
