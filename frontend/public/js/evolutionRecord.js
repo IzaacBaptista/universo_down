3187d72ec4d05af75ec279a0a45e9e37a54da9f2
luiz-fischer <luiz.fischer@alunos.sc.senac.br>
2021-11-25 17:35:03 -0300

    fix: date logic

--- frontend/public/js/evolutionRecord.js
+++ frontend/public/js/evolutionRecord.js
@@ -1,36 +1,33 @@
 var baseUrlApi = "http://localhost:3000/api/v1";
 import { erroHandler } from "../js/fns/erroHandler.js";
 import { checkProperties } from "../js/fns/checkForm.js";
+import { ajaxRequest } from "../js/fns/ajaxRequest";
 
 $(document).ready(function () {
   //height
   $("#height").mask("9.99");
-
+  window.localStorage.removeItem("userId");
   window.localStorage.removeItem("assistedId");
   $("#form").submit(function(e) {
     e.preventDefault();
     var assistedId = localStorage.getItem("assistedId");
+    var userId = localStorage.getItem("userId");
     var date = document.getElementById("date");
     var status = $("#status :selected").val();
     var weight = document.getElementById("weight");
     var height = document.getElementById("height");
     var report = document.getElementById("report");
 
-    var dateNow = new Date().toLocaleDateString();
-
-    if (date.value >= dateNow) {
       var data = {
         assistedId: assistedId,
+        userId: userId,
         date: date.value,
         status: status,
         weight: weight.value,
         height: height.value,
         report: report.value,
       };
-    } else {
-      alert("Data Inválida");
-    }
-
+   
     if (checkProperties(data) === false) {
       try {
           $.ajax({
